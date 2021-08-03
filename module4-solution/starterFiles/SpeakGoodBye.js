69abe0b71fad8e0c394f88e35212b09621408273
Helene_Rinn <helene.rinn@web.de>
2021-08-03 03:41:34 +0200

    verbesserung

--- module4-solution/starterFiles/SpeakGoodBye.js
+++ module4-solution/starterFiles/SpeakGoodBye.js
@@ -8,12 +8,13 @@
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
+
 var byeSpeaker = {};
-	byeSpeaker.speak = function speak(name) {
+var speakWord = "Good Bye";
+	byeSpeaker.speak = function (name) {
 		console.log(speakWord + " " + byeSpeaker.name);
 	}
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
-var speakWord = "Good Bye";
 
 
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
--- module4-solution/starterFiles/SpeakHello.js
+++ module4-solution/starterFiles/SpeakHello.js
@@ -5,13 +5,15 @@
 	// STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 	// the "speak" method and which you will expose to the global context
 	// See Lecture 52, part 1
+
 	var helloSpeaker = {};
-	helloSpeaker.speak = function speak(name) {
+	var speakWord = "Hello";
+	helloSpeaker.speak = function (name) {
 		console.log(speakWord + " " + helloSpeaker.name);
 	}
 
 	// DO NOT attach the speakWord variable to the 'helloSpeaker' object.
-	var speakWord = "Hello";
+
 
 	window.helloSpeaker = helloSpeaker;
 
